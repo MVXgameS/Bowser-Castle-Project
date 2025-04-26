086e2a3cb839e2bb06d1c724cf5b4fdde75ed909
MVXgameS <maxliburdi08@gmail.com>
2025-04-26 11:13:26 -0400

    Add files via upload

--- tileDescriptions.js
+++ tileDescriptions.js
@@ -6,6 +6,97 @@ function createTileMap(coords, description) {
   }
   
   const tileDescriptionsByGFX = {
+    "Fire Hazards 1":{
+    //Big Sparky
+      ...createTileMap(
+        ["0_0", "0_32", "0_64", "0_96", "32_0", "32_32", "32_64", "32_96", 
+          "64_0", "64_32", "64_64", "64_96", "96_0", "96_32", "96_64", "96_96"
+        ],
+        {
+          usage: "Used for the Big Sparky Frame 1/8",
+          palette: "Palette A",
+          type: "Castle",
+          mode: "4BPP SNES"
+        }
+      ),
+      ...createTileMap(
+        ["128_0", "128_32", "128_64", "128_96", "160_0", "160_32", "160_64", "160_96", 
+          "192_0", "192_32", "192_64", "192_96", "224_0", "224_32", "224_64", "224_96"
+        ],
+        {
+          usage: "Used for the Big Sparky Frame 2/8",
+          palette: "Palette A",
+          type: "Castle",
+          mode: "4BPP SNES"
+        }
+      ),
+      ...createTileMap(
+        ["256_0", "256_32", "256_64", "256_96", "288_0", "288_32", "288_64", "288_96", 
+          "320_0", "320_32", "320_64", "320_96", "352_0", "352_32", "352_64", "352_96"
+        ],
+        {
+          usage: "Used for the Big Sparky Frame 3/8",
+          palette: "Palette A",
+          type: "Castle",
+          mode: "4BPP SNES"
+        }
+      ),
+      ...createTileMap(
+        ["384_0", "384_32", "384_64", "384_96", "416_0", "416_32", "416_64", "416_96", 
+          "448_0", "448_32", "448_64", "448_96", "480_0", "480_32", "480_64", "480_96"
+        ],
+        {
+          usage: "Used for the Big Sparky Frame 4/8",
+          palette: "Palette A",
+          type: "Castle",
+          mode: "4BPP SNES"
+        }
+      ),
+      ...createTileMap(
+        ["0_128", "0_160", "0_192", "0_224", "32_128", "32_160", "32_192", "32_224", 
+          "64_128", "64_160", "64_192", "64_224", "96_128", "96_160", "96_192", "96_224"
+        ],
+        {
+          usage: "Used for the Big Sparky Frame 5/8",
+          palette: "Palette A",
+          type: "Castle",
+          mode: "4BPP SNES"
+        }
+      ),
+      ...createTileMap(
+        ["128_128", "128_160", "128_192", "128_224", "160_128", "160_160", "160_192", "160_224", 
+          "192_128", "192_160", "192_192", "192_224", "224_128", "224_160", "224_192", "224_224"
+        ],
+        {
+          usage: "Used for the Big Sparky Frame 6/8",
+          palette: "Palette A",
+          type: "Castle",
+          mode: "4BPP SNES"
+        }
+      ),
+      ...createTileMap(
+        ["256_128", "256_160", "256_192", "256_224", "288_128", "288_160", "288_192", "288_224", 
+          "320_128", "320_160", "320_192", "320_224", "352_128", "352_160", "352_192", "352_224"
+        ],
+        {
+          usage: "Used for the Big Sparky Frame 7/8",
+          palette: "Palette A",
+          type: "Castle",
+          mode: "4BPP SNES"
+        }
+      ),
+      ...createTileMap(
+        ["384_128", "384_160", "384_192", "384_224", "416_128", "416_160", "416_192", "416_224", 
+          "448_128", "448_160", "448_192", "448_224", "480_128", "480_160", "480_192", "480_224"
+        ],
+        {
+          usage: "Used for the Big Sparky Frame 8/8",
+          palette: "Palette A",
+          type: "Castle",
+          mode: "4BPP SNES"
+        }
+      ),
+    },
     "Enemies 2": {
     // Parachuting Bom-omb
       ...createTileMap(
